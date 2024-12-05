2b4ca9150612801201034d0f9593602c78268df6
ViktorBach <vmb0107@gmail.com>
2024-12-05 14:41:23 +0100

    added connection to lejeaftaleservice

--- app.py
+++ app.py
@@ -15,20 +15,24 @@ db_path = os.path.join(script_dir, 'Database/rapport.db')
 
 @app.route('/udlejedeBiler', methods=['GET'])
 def udlejedeBiler():
-    response = requests.get("http://localhost:5002/lejeaftale")
-    if response.status_code != 200:
+    lejeaftale_response = requests.get("http://localhost:5002/lejeaftale")
+    if lejeaftale_response.status_code != 200:
         return jsonify({"error": "Failed to fetch data from Lejeaftale microservice"}), 500
     
-    lejeaftale_data = response.json()
-    
-    available_cars = []
+    lejeaftale_data = lejeaftale_response.json()
+
+    rented_cars= []
     for car in lejeaftale_data:
         bil_id = car['BilID']
-        pris_pr_måned = car['PrisPrMåned']
+        kunde_id = car['KundeID']
+        
         status_response = requests.get(f"http://localhost:5002/status/{bil_id}")
         if status_response.status_code == 200 and status_response.json().get('status') == 'Aktiv':
-            available_cars.append({"bil_id": bil_id, "pris_pr_måned": pris_pr_måned})
+                rented_cars.append({
+                    "bil_id": bil_id,
+                    "kunde_id": kunde_id,
+                })
     
-    return jsonify({"available_cars": available_cars}), 201
+    return jsonify({"rented_cars": rented_cars}), 201
 
-app.run(debug=True, host='0.0.0.0', port=5003)
+app.run(debug=True, host='0.0.0.0', port=5001)
