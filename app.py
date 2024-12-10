c8eb428263044ef8ad99b03fc58eeea90b78eba4
ViktorBach <vmb0107@gmail.com>
2024-12-10 13:41:00 +0100

    add data to rapport.db

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ app = Flask(__name__)
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, 'Database/rapport.db')
 
+rented_cars = []
+
+
 @app.route('/udlejedeBiler', methods=['GET'])
 def udlejedeBiler():
+    global rented_cars
     lejeaftale_response = requests.get("http://localhost:5002/lejeaftale")
     if lejeaftale_response.status_code != 200:
         return jsonify({"error": "Failed to fetch data from Lejeaftale microservice"}), 500
@@ -34,5 +38,22 @@ def udlejedeBiler():
                 })
     
     return jsonify({"rented_cars": rented_cars}), 201
+
+@app.route('/gemUdlejedeBiler', methods=['POST'])
+def gemUdlejedeBiler():
+    global rented_cars
+    if len(rented_cars) == 0:
+        return jsonify({"error": "No rented cars to save"}), 400
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO Rapport (Antal_Udlejede_Biler) VALUES (?)", (len(rented_cars),))
+    
+    conn.commit()
+    conn.close()
+
+    rented_cars = []
+    return jsonify({"message": "Rented cars saved"}), 201
 
 app.run(debug=True, host='0.0.0.0', port=5001)
